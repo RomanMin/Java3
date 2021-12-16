0c1184e3d192fcc4cfe17c8602f21d9d2ec9f36e
RomanMin <1@ali.m-rv.ru>
2021-12-16 21:17:29 +0300

    HomeWork4

---
+++ src/test/java/com/geekbrains/java3/TriangleTest.java
@@ -0,0 +1,30 @@
+package com.geekbrains.java3;
+
+import HomeWork4.NotTriangle;
+import HomeWork4.TriangleSquare;
+import org.junit.Test;
+import org.junit.jupiter.api.BeforeAll;
+import org.slf4j.LoggerFactory;
+
+import java.util.logging.Logger;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class TriangleTest {
+
+/* private static Logger logger = (Logger) LoggerFactory.getLogger(TriangleSquare.class);
+
+    @BeforeAll
+    public void beforeAll() {
+       logger.info("Метод выполнился 1 раз, перед всеми тестами");
+       logger.trace("trace level log");
+       logger.error("error log message");
+
+    }*/
+// c логгером тест всегда падает :-( поэтому строки выше закомментированы
+    @Test
+    public void exceptionNotTriangle() {
+     assertThatExceptionOfType(NotTriangle.class).isThrownBy(() -> TriangleSquare.squareIsValid(13, 4, 5));
+    }
+
+}
